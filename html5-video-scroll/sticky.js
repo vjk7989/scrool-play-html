b1315bb0ba9737c957194ee5b70564ccf07ff7df
vjk7989 <92521801+vjk7989@users.noreply.github.com>
2023-12-27 22:48:09 +0530

    Add files via upload

---
+++ html5-video-scroll/sticky.js
@@ -0,0 +1,27 @@
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
+
+var frameNumber = 0, // start video at frame 0
+    // lower numbers = faster playback
+    playbackConst = 450, 
+    // get page height from video duration
+    setHeight = document.getElementById("set-height"), 
+    // select video element         
+    vid = document.getElementById('v0'); 
+
+// dynamically set the page height according to video length
+vid.addEventListener('loadedmetadata', function() {
+  setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
+});
+
+// Use requestAnimationFrame for smooth playback
+function scrollPlay(){  
+  var frameNumber  = window.pageYOffset/playbackConst;
+  vid.currentTime  = frameNumber;
+  window.requestAnimationFrame(scrollPlay);
+}
+
+window.requestAnimationFrame(scrollPlay);
+
